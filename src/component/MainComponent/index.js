3fb7fd1de00b2a169f0bd4df7a310e3ad653ae17
manjeet6972 <manjeet6972@gmail.com>
2021-07-28 22:37:05 +0530

    Header dropdown added

--- src/component/MainComponent/index.js
+++ src/component/MainComponent/index.js
@@ -1,19 +1,24 @@
 import React from 'react'
-import { Layout,Breadcrumb } from 'antd';
+import { Layout, Breadcrumb,Typography } from 'antd';
 import './index.css';
 
-const { Header, Content } = Layout;
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 const MainComponent = () => {
+
 	return (
 		<Layout className="site-layout">
 			<Header className="site-layout-background">
-
+				<select className="dropdown-menu">
+					<option value="department">Select a Department</option>
+				</select>
+				<Title level={5} className="en">EN</Title>
 			</Header>
 			<Breadcrumb className="breadcrumb">
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item style={{color: 'darkgreen'}}>Financial Overview</Breadcrumb.Item>
-          
-        </Breadcrumb>
+				<Breadcrumb.Item>Home</Breadcrumb.Item>
+				<Breadcrumb.Item style={{ color: 'darkgreen' }}>Financial Overview</Breadcrumb.Item>
+			</Breadcrumb>
+			<Title level={4} className="sheet">Sheet 1</Title>
 			<Content
 				className="site-layout-background"
 				style={{
@@ -24,6 +29,7 @@ const MainComponent = () => {
 			>
 				Content
 			</Content>
+			<Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
 		</Layout>
 	)
 }
